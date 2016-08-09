a58c8836647ba6f7bc64f8414c2dcad3acc876ec
yzzting <yzz1995@aliyun.com>
2016-08-09 17:10:22 +0800

    Update 2016-08-09T09:09:59.406Z

--- app.js
+++
@@ -1,9 +0,0 @@
-var superagent = require('superagent');
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.listen(80);
-console.log('sudo node app => 80');
